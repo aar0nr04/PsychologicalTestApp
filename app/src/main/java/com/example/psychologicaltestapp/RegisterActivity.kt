acdead3a1aa48aa4423b7406dba4c9efbad3dc82
aar0nr04 <aar0nr@hotmail.com>
2025-07-07 14:35:45 -0600

    - Working version - Profile is no working

--- app/src/main/java/com/example/psychologicaltestapp/RegisterActivity.kt
+++ app/src/main/java/com/example/psychologicaltestapp/RegisterActivity.kt
@@ -16,6 +16,9 @@ class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+
         setContentView(R.layout.activity_register)
 
         // Inicializar repositorios
