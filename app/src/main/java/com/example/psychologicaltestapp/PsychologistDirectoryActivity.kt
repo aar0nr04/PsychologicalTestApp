5782a81e44405016d4b7ae92801841667ca1ed1a
aar0nr04 <aar0nr@hotmail.com>
2025-10-28 09:58:08 -0600

    Allow AppointmentItem callers to omit startTimeText

--- app/src/main/java/com/example/psychologicaltestapp/PsychologistDirectoryActivity.kt
+++ app/src/main/java/com/example/psychologicaltestapp/PsychologistDirectoryActivity.kt
@@ -8,12 +8,14 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 
 class PsychologistDirectoryActivity : BaseActivity() {
 
@@ -28,6 +30,12 @@ class PsychologistDirectoryActivity : BaseActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_psychologist_directory)
+
+        MobileAds.initialize(this)
+        findViewById<AdView?>(R.id.adView)?.let { adView ->
+            val adRequest = AdRequest.Builder().build()
+            adView.loadAd(adRequest)
+        }
 
         recyclerView = findViewById(R.id.recyclerView)
         layoutManager = LinearLayoutManager(this)
